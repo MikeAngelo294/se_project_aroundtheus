1b9ece6e21832b4de5bdfde90a3695e606afde5e
Michael Sanchez <mikesanchezgow@gmail.com>
2024-05-04 00:48:18 -0500

    Project 4 Stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,14 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const closedModalButton = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+closedModalButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
