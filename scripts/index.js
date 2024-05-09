f194713a6756a5cd33dd500eb4e70495dabcc8b2
Michael Sanchez <mikesanchezgow@gmail.com>
2024-05-09 13:04:16 -0500

    Project 4 Final

--- scripts/index.js
+++ scripts/index.js
@@ -42,7 +42,7 @@ const cardTemplate =
   document.querySelector("#card-template").content.firstElementChild;
 
 function closePopUp() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -71,7 +71,7 @@ function handleProfileEditSubmit(event) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 closedModalButton.addEventListener("click", closePopUp);
 
