dfd7c45225693ab07aaa9ed16f65c74d25362d49
Michael Sanchez <mikesanchezgow@gmail.com>
2024-10-14 15:49:25 -0500

    Project 8

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ export default class PopupWithForm extends Popup {
   constructor({ popupSelector, handleFormSubmit }) {
     super({ popupSelector });
     this._form = this._popupElement.querySelector(".modal__form");
-    this._inputListing = this._popupElement.querySelectorAll(".modal__input");
+    this._inputList = this._popupElement.querySelectorAll(".modal__input");
     this._handleFormSubmit = handleFormSubmit;
     this._submitButton = this._form.querySelector(".modal__button");
   }
@@ -12,7 +12,7 @@ export default class PopupWithForm extends Popup {
   //data from all input fields
   _getInputValues() {
     this.inputValue = {};
-    this._inputListing.forEach((input) => {
+    this._inputList.forEach((input) => {
       this.inputValue[input.name] = input.value;
     });
 
